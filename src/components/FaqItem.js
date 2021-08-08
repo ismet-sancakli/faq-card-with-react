b50e6c03bb0b431440b126ed5c31d74e4c31c3d0
İsmet Sancaklı <45608091+ismet-sancakli@users.noreply.github.com>
2021-08-08 15:55:37 +0300

    Update FaqItem.js

--- src/components/FaqItem.js
+++ src/components/FaqItem.js
@@ -1,21 +1,15 @@
 import { useState } from "react";
-
-const FaqItem = ({id, question, children}) => {
-  
+const FaqItem = ({id, question, children}) => {  
   const [isvisible, setVisible]= useState(false);  
     
-  return (
-        
+  return (        
     <div className="faq-item">
       <input type="checkbox" id={id}/>
-      <label for={id} onClick= {() => setVisible(!isvisible)}>{question}<span><img src="./images/icon-arrow-down.svg" alt=""/></span></label>
-      
+      <label for={id} onClick= {() => setVisible(!isvisible)}>{question}<span><img src="./images/icon-arrow-down.svg" alt=""/></span></label>      
       {
         isvisible ? <div className="content">{children}</div> : null
       } 
-    </div>
-      
+    </div>      
   );
 }
-
 export default FaqItem;
